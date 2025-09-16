9065d5a6610987fb5cd9cd11201b77c3b9e61ce1
jobchoi <sedcreta12@gmail.com>
2025-09-16 12:23:20 +0900

    c++ 반복문 숫자를 다섯개 출력해보기

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+    // for문
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        std::cout<< "cnt :"<<i <<std::endl;
+    }
+
+    std::cout<< "cnt =======================>>  :"<<std::endl;
+
+    // for문
+    for (int i = 1; i < 6; i++)
+    {
+        /* code */
+        std::cout<< "cnt :"<<i <<std::endl;
+    }
+    return 0;
+}
