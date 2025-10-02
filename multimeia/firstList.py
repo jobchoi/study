270f1acb1094bf0b8b360f8634229ee5f473f72d
jobchoi <sedcreta12@gmail.com>
2025-10-02 11:59:47 +0900

    list 예제 따라해보기 - 1

---
+++ multimeia/firstList.py
@@ -0,0 +1,37 @@
+# dogNames=[]
+
+# while True:
+#     name = input()
+
+#     if name =='':
+#         break
+#     dogNames.append(name)
+
+
+# print('강아지들의 이름')
+
+
+# for name in dogNames:
+#     print(name, end=',')
+
+# text = "will is power."
+
+# print(text[0], text[3],text[-1])
+
+# flist = ["apple","banana","tomato","peach","pear"]
+# print(flist[0], flist[3], flist[-1])
+
+# 리스트를 중간에서 변경하기
+
+# letters = ['a','b','c','d','d','e','f','g']
+# print(letters)
+# letters[2:5] = ['C','D','E']
+# print(letters)
+
+# letters[2:5] = []
+# print("111==>",letters)
+
+# list[start : stop : step]
+numbers = [ 10, 20, 30, 40, 50, 60, 70, 80, 90 ]
+
+print(numbers[::-1])
