ff0d1692f012f1f9362f6a3ab79455eb25cdfc95
Kojihye1102 <adxcb@naver.com>
2025-10-13 20:19:15 +0900

    기본예제부터 단계별 도전해보기

--- multimedia/onlineJudge.py
+++ multimedia/onlineJudge.py
@@ -1,7 +1,40 @@
-# 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성
+# # 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성
 
-# 엔터를 구분으로 map 없이 코드를 구현한것은 에러는 없지만, 채점시 한줄에 스페이스바로 구분하여서 map으로 수정함
-numA,numb  = map(int,(input().split()))
+# # 엔터를 구분으로 map 없이 코드를 구현한것은 에러는 없지만, 채점시 한줄에 스페이스바로 구분하여서 map으로 수정함
+# numA,numb  = map(int,(input().split()))
 
+# print((float)(numA / numb))
 
-print((float)(numA / numb))
+# # 사칙연산 예제 - 두개의 수 입력 -> 입력받은 수를 가지고 6가지 연산하기
+# numA,numB  = map(int,(input().split()))
+# print(numA + numB)
+# print(numA - numB)
+# print(numA * numB)
+# print((int)(numA / numB))
+# print((int)(numA % numB))
+
+# # 입력된 문자열에 문자열을 붙이기
+# getStr = input()
+# print(getStr+"??!")
+
+# # 서기 연도를 불기 연도로 변경해보기
+# getYear=(int)(input())
+# print(getYear-543)
+
+# # 변수 3개 덧셈
+# numA,numB,numC  = map(int,(input().split()))
+# print(numA+numB+numC)
+
+# 고양이 출력하기
+# print('\    /\\')
+# print(" )  ( ')")
+# print("(  /  )")
+# print(" \(__)|")
+
+# 개 출력
+
+# print("|\\_/|")
+# print("|q p|   /}")
+# print('( 0 )"""\\')
+# print('|"^"`    |')
+# print("||_/=\\\\__|")
