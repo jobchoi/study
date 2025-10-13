f3066d1aceb42f3260754d3a306805dfc2810cb8
jobchoi <sedcreta12@gmail.com>
2025-10-13 11:04:27 +0900

    클래스내 메서드 오버라이딩해서 리스트로 이름 입력 받아보기

--- multimeia/chap6.py
+++ multimeia/chap6.py
@@ -20,37 +20,61 @@
 # print(f"리스트 {example_list}의 합 : {resultSum}")
 
 
-STUDENT = 3
+# STUDENT = 3
 
-def getScoData():
-    getIntScores = []
+# def getScoData():
+#     getIntScores = []
     
-    # 다른 조건없이 반복횟수만 주고 싶을 때  '_'은 관례적인 변수
-    for _ in range(STUDENT):
-        getIntScores.append(int(input("성적입력 : ")))                
+#     # 다른 조건없이 반복횟수만 주고 싶을 때  '_'은 관례적인 변수
+#     for _ in range(STUDENT):
+#         getIntScores.append(int(input("성적입력 : ")))                
 
-    return getIntScores
+#     return getIntScores
 
 
-def avgCal(getScoList):
-    resultAvg = 0
+# def avgCal(getScoList):
+#     resultAvg = 0
 
-    # C와는 다르게 인덱스가 아닌 값을 가지고 반복문 연산을 수행
-    # buf(변수명은 사용자가 알아서) 변수에 파라미터 값을 꺼내 저장
-    for buf in getScoList:  
+#     # C와는 다르게 인덱스가 아닌 값을 가지고 반복문 연산을 수행
+#     # buf(변수명은 사용자가 알아서) 변수에 파라미터 값을 꺼내 저장
+#     for buf in getScoList:  
         
-        # 파라미터(여기서는 list)에서 꺼낸 값을 저장한 변수를 가지고, 리턴 변수에 누적합을 수행
-        resultAvg += buf    
+#         # 파라미터(여기서는 list)에서 꺼낸 값을 저장한 변수를 가지고, 리턴 변수에 누적합을 수행
+#         resultAvg += buf    
     
-    resultAvg = resultAvg / STUDENT
+#     resultAvg = resultAvg / STUDENT
     
-    return resultAvg
+#     return resultAvg
+
+# resultScores = avgCal(getScoData())
+
+# print(f"Result : {resultScores}")
+
+
+# 클래스
+class Animal:
+# 이름을 입력받는 함수생성
+    def getName(self):
+        animalName = input("이름을 입력해주세요")
+        return animalName
 
 
 
+# 메서드 오버라이딩 연습하기
+class dogs(Animal): # Animal 클래스 상속
+
+    CNT = 3
+
+    def getName(self):
+        namelist = []
+
+        for _ in range(self.CNT):
+            namelist.append( super().getName()) # super().getName() -> Animal에 getName()
+
+        print(f"dogs Name : {namelist}")
+
+        
 
 
-resultScores = avgCal(getScoData())
-
-print(f"Result : {resultScores}")
-
+test = dogs()
+test.getName()
