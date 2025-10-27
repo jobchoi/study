6d87be2e35a45187ff41ed98c3bb40235ddcb14e
Kojihye1102 <adxcb@naver.com>
2025-10-27 19:19:44 +0900

    cv.split로 분리해보기, numpy로 해보기

--- courses/multimedia/cv1027.py
+++ courses/multimedia/cv1027.py
@@ -1,14 +1,22 @@
-import numpy
+import numpy as np
 import cv2 as cv
 
+# 1. 3채널 이미지를 
+# 2. numpy를 이용해서, img를 잘라 본다.
+# 3. 그게 익숙해지면, 행(r),열(c) 번호를 입력받아서 부분부분 출력하는거 까지.
+
 img = cv.imread('soccer.jpg')
+b,g,r = cv.split(img)
 print(img.shape)
-print("1")
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-print("2")
-
+# imgB = cv.imread()
+print(type(img))
 
 cv.imshow('',gray)
-print("3")
+cv.imshow('blue',b)
+cv.imshow('green',g)
+cv.imshow('red',r)
+
 cv.waitKey(0)
+
