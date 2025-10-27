1e39287bc5014d2e148f214b462e3da2a0b0fd1f
Kojihye1102 <adxcb@naver.com>
2025-10-27 19:36:17 +0900

    3채널로 나눠보기 완료, numpy로 파일 받아보기 시작

--- courses/multimedia/cv1027.py
+++ courses/multimedia/cv1027.py
@@ -6,17 +6,29 @@ import cv2 as cv
 # 3. 그게 익숙해지면, 행(r),열(c) 번호를 입력받아서 부분부분 출력하는거 까지.
 
 img = cv.imread('soccer.jpg')
-b,g,r = cv.split(img)
-print(img.shape)
+# cv.split
+# b,g,r = cv.split(img)
+
+# numpy로 해보기
+# test0=img[::0]
+test1=img[:,:,1]
+test2=img[:,:,2]
+test0=img[:,:,0]
+
+print("img.shape : ",img.shape)
+print("test1.shape : ",test1.shape)
+cv.imshow('t1',test1)
+cv.imshow('t2',test2)
+cv.imshow('t0',test0)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 # imgB = cv.imread()
 print(type(img))
 
-cv.imshow('',gray)
-cv.imshow('blue',b)
-cv.imshow('green',g)
-cv.imshow('red',r)
+# cv.imshow('',gray)
+# cv.imshow('blue',b)
+# cv.imshow('green',g)
+# cv.imshow('red',r)
 
 cv.waitKey(0)
 
