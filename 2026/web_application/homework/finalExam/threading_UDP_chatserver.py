e282b22c9306330ead5338170c7d59b0b2a4ee36
jobchoi <sedcreta12@gmail.com>
2026-06-13 13:21:25 +0900

    server

---
+++ 2026/web_application/homework/finalExam/threading_UDP_chatserver.py
@@ -0,0 +1,32 @@
+import  socket
+import time
+
+HOST = ''
+PORT = 2500
+BUFFER_SIZE = 1024  
+
+clients = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((HOST, PORT))
+s.setblocking(0)
+
+print("Server Started...")
+
+while True:
+    try:
+        data, addr = s.recvfrom(BUFFER_SIZE)
+        if "quit" in data.decode():
+            if addr in clients:
+                clients.remove(addr)
+                print(f"==> {addr} is lost.")
+            if addr not in clients :
+                print(f"new client {addr} is connected.")
+                clients.append(addr)
+            print(time.ctime(time.time()) + str(addr) + ':'+ data.decode())
+            for client in clients:
+                if client != addr:
+                    s.sendto(data, client)
+    except :
+        pass
+    s.close()   
